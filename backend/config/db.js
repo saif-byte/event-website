d813177f9a7832680ee14b9576384a947346f032
Sofstica-Saifullah-Khan <saifullah.khan@sofstica.com>
2025-04-06 21:57:35 +0500

    uri to env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const mongoose = require("mongoose");
 
-const MONGO_URI = "mongodb://localhost:27017" ;
+const MONGO_URI = process.env.MONGO_URI ;
 
 mongoose
   .connect(MONGO_URI)
