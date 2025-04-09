3437826aa60ada605d7434e907e7432fe74e1086
Saifullah Khan <saif4rmkhan@gmail.com>
2025-04-09 23:22:50 +0500

    api change

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import { toast } from 'react-toastify';
 import { useLoader } from '../context/LoaderContext';
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5001/api";
+const API_BASE_URL = "https://sandiegoadventurehub.onrender.com/api";
 
 export const apiCall = async (endpoint, method = "GET", data = null) => {
   
